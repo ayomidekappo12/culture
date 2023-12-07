2684a59abe6519e79d3ad0fa615f663ad4b699ce
ayomide kappo <118881021+ayomidekappo12@users.noreply.github.com>
2023-12-07 19:54:04 +0100

    added new files

--- coulture/task_1/login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ coulture/task_1/login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./LoginSignup.css";
+import Email_icon from "../Assets/Email.png";
+import Password_icon from "../Assets/padlock.png";
+import User_icon from "../Assets/person.png";
 
 const LoginSignup = () => {
-  return <div>{}</div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={User_icon} alt="" />
+          <input type="name" />
+        </div>
+        <div className="input">
+          <img src={Email_icon} alt="" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src={Password_icon} alt="" />
+          <input type="password" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
