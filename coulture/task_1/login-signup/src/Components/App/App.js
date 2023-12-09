a01643a509ee788295881f9f3720605f9a930c6e
ayomide kappo <118881021+ayomidekappo12@users.noreply.github.com>
2023-12-09 16:09:14 +0100

    added new file

--- coulture/task_1/login-signup/src/Components/App/App.js
+++ coulture/task_1/login-signup/src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../header/header";
+import Billboard from "../billboard/billboard";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <div className="heading-section">
           <Header />
+          <Billboard />
         </div>
       </div>
     </React.Fragment>
