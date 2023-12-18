c6162507e7592df64150e73e3098c6db7f6de8d7
ayomide kappo <118881021+ayomidekappo12@users.noreply.github.com>
2023-12-18 20:00:25 +0100

    added new files

--- coulture/task_2/login-signup/src/LoginSignup/LoginSignup.test.js
+++ coulture/task_2/login-signup/src/LoginSignup/LoginSignup.test.js
@@ -12,37 +12,29 @@ describe("LoginSignup Component", () => {
     const wrapper = shallow(<LoginSignup />);
 
     // Ensure the default state is as expected
-    expect(wrapper.state().action).toBe("Sign Up");
-
-    // Ensure the initial rendered text is correct
-    expect(wrapper.find(".text").text()).toBe("Sign Up");
+    const initialAction = wrapper.find(".text").text();
+    expect(initialAction).toBe("Sign Up");
   });
 
   it("changes action state on Sign Up click", () => {
     const wrapper = shallow(<LoginSignup />);
 
-    // Simulate a click on the "Sign Up" button
-    wrapper.find(".submit").at(0).simulate("click");
-
-    // Ensure the state is updated correctly
-    expect(wrapper.state().action).toBe("Sign Up");
+    // Find the Sign Up button and simulate a click
+    wrapper.find(".submit-container button").at(0).simulate("click");
 
     // Ensure the rendered text is updated correctly
-    expect(wrapper.find(".text").text()).toBe("Sign Up");
+    const updatedAction = wrapper.find(".text").text();
+    expect(updatedAction).toBe("Sign Up");
   });
 
   it("changes action state on Login click", () => {
     const wrapper = shallow(<LoginSignup />);
 
-    // Simulate a click on the "Login" button
-    wrapper.find(".submit").at(1).simulate("click");
-
-    // Ensure the state is updated correctly
-    expect(wrapper.state().action).toBe("Login");
+    // Find the Login button and simulate a click
+    wrapper.find(".submit-container button").at(1).simulate("click");
 
     // Ensure the rendered text is updated correctly
-    expect(wrapper.find(".text").text()).toBe("Login");
+    const updatedAction = wrapper.find(".text").text();
+    expect(updatedAction).toBe("Login");
   });
-
-  // Add more test cases for other functionalities as needed
 });
